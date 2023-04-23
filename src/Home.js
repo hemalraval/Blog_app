2efb68f64b597246bff989e2dd9d12ea2cd852b7
hemalraval <hemal8198@gmail.com>
2023-04-23 18:50:11 -0400

    Sample Blog Listing 10

--- src/Home.js
+++ src/Home.js
@@ -2,18 +2,22 @@ import { useState } from "react";
 
 const Home = () => {
 
-  const[name, setName] = useState("Mario");
+  const[blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
 
-  const handleClick = () => {
-    setName("Hemal");
-  };
-
+  
     return (
-      <div className="home">
-        <h2>Homepage</h2>
-        <h1>My name is: { name }</h1>
-        <button onClick={handleClick}>Click Me</button>
-      </div>
+    <div className="home">
+      {blogs.map(blog => (
+        <div className="blog-preview" key={blog.id} >
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
+    </div>
     );
   }
    
