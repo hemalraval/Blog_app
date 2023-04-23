e265c073a81c521676cbfde51686add03b0d9701
hemalraval <hemal8198@gmail.com>
2023-04-22 22:37:30 -0400

    Second commit project setup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-
-  const xx = {
-    name:"a",
-    age:44
-  };
-
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
-      <div className="component">
-        <h3>My Component</h3>
-        <h1>{ xx.name }</h1>
+      <Navbar />
+      <div className="content">
+        <Home />
       </div>
     </div>
   );
-} 
+}
 
 export default App;
